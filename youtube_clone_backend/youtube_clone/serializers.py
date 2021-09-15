f7e10ffade220fc03c8a0179de9c799cb2d1854f
Matthew Keplinger <matthew.keplinger85@gmail.com>
2021-09-15 15:08:43 -0400

    Added serializers.py for comment, reply; changed settings

---
+++ youtube_clone_backend/youtube_clone/serializers.py
@@ -0,0 +1,13 @@
+from django.db import models
+from rest_framework import serializers
+from .models import Comment, Reply
+
+class CommentSerializer(serializers.ModelSerializer):
+    class Meta: 
+        model = Comment
+        fields = ['id', 'video_id', 'body', 'likes', 'dislikes']
+
+class ReplySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Reply
+        fields = ['comment', 'body']
--- youtube_clone_backend/youtube_clone_backend/settings.py
+++ youtube_clone_backend/youtube_clone_backend/settings.py
@@ -19,8 +19,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-qvcuq02^mx)r3j-8y5xibn$+)i9wyg+(r%wu6+k^pw1zp7xzki'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
